ccac9f9225c142eebd0451e09c2862cbae168c34
nannany <ymym1990ymym@gmail.com>
2020-02-01 17:16:04 +0900

    かんきょうへんすうで取れることがわかった

--- src/main/java/nannany/thymeleaf/controller/ThymeleafController.java
+++ src/main/java/nannany/thymeleaf/controller/ThymeleafController.java
@@ -1,14 +1,18 @@
 package nannany.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ThymeleafController {
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";
+    @GetMapping("/app")
+    public String home(Model model) {
+
+        model.addAttribute("test", "lll");
+
+        return "/index.html";
 
     }
 }
