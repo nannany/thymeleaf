78a863dbc034467402b7c27a8ab19a5047040dbc
nannany <ymym1990ymym@gmail.com>
2020-03-07 20:15:28 +0900

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Environment} from "./Environment";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Environment />
+      </header>
+    </div>
+  );
+}
+
+export default App;
